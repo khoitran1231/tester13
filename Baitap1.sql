c5c94659c9c72734d063a2d6e07b7d205a7738f8
khoitran1231 <108212193+khoitran1231@users.noreply.github.com>
2023-02-11 12:07:00 +0700

    Add files via upload

---
+++ Baitap1.sql
@@ -0,0 +1,25 @@
+-- bỏ database
+drop database if exists fresher_training_management;
+-- tạo database mới
+create database fresher_training_management;
+-- sử dụng database cụ thể
+use fresher_training_management;
+
+
+-- tạo bảng Trainee
+create table trainee(
+	TraineeID INT PRIMARY KEY auto_increment NOT NULL,
+    Full_Name VARCHAR(50) NOT NULL,
+    Birth_Date Date NOT NULL,
+    Gender ENUM('male', 'female', 'unknown'),
+    ET_IQ tinyint check(ET_IQ >= 0 AND ET_IQ <=20),
+    ET_Gmath tinyint check(ET_Gmath >= 0 AND ET_Gmath <=20),
+    ET_English tinyint check(ET_English >= 0 AND ET_English <=50),
+    Training_Class VARCHAR(50) NOT NULL,
+    Evaluation_Notes VARCHAR(250)
+);
+
+
+-- thêm cột VTI_Account vào bảng Trainee
+ALTER TABLE Trainee
+ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;
